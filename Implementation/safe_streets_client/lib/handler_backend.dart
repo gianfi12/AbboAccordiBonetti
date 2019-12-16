17b2a2f3003bfdc13c3a7c137b034d639c489405
gianfi <10587213@polimi.it>
2019-12-16 18:53:06 +0100

    Updated login method

--- Implementation/safe_streets_client/lib/handler_backend.dart
+++ Implementation/safe_streets_client/lib/handler_backend.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/foundation.dart';
 
 import 'handler_model.dart' as model;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+
 
 //TODO: implement backend
 //TODO: handle errors
 
 /// Returns a future containing whether the login was successful.
-Future<bool> login({@required String username, @required String password}) {
+Future<bool> login({@required String username, @required String password}) async{
+  String soap = '''<?xml version='1.0' encoding='UTF-8'?><S:Envelope xmlns:S="http://schemas.xmlsoap.org/soap/envelope/"><S:Body><ns2:userRegistration xmlns:ns2="http://SafeStreets.com/"><arg0>{"username":"Gian","email":"","firstName":"","lastName":"","placeOfBirth":{},"placeOfResidence":{},"fiscalCode":"","dateOfBirth":"Dec 16, 2019 4:44:10 PM","password":""}</arg0><arg1></arg1></ns2:userRegistration></S:Body></S:Envelope>''';
+
+  http.Response response = await http.post(
+    'http://10.42.0.1:8080/SafeStreetsSOAP/DispatcherService',
+    headers: {
+      'content-type': 'text/xml',
+      'SOAPAction': 'http://SafeStreets.com/Dispatcher/userRegistrationRequest',
+    },
+    body: utf8.encode(soap),
+  );
+  print(response.body);
   return Future.delayed(Duration(seconds: 1), () => true);
 }
 
